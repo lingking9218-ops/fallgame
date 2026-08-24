5d65b1cfd1290d2f10885b3bffd1afbc75eacc43
lingking9218-ops <lingking9218@gmail.com>
2026-08-24 13:58:57 +0800

    Create game.js

---
+++ game.js
@@ -0,0 +1,7 @@
+document.getElementById("createRoom").addEventListener("click", () => {
+    alert("建立房間！");
+});
+
+document.getElementById("joinRoom").addEventListener("click", () => {
+    alert("加入房間！");
+});
